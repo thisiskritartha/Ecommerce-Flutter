83269327238caff4573617cecfc1ab5853e127ca
kritartha bikram shah <thisiskritartha@gmail.com>
2023-02-09 21:53:32 +0545

    Connected to mongoDb, created the user Routes and user Controller

--- backend_service/server.js
+++ backend_service/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const userRouter = require('./Routes/userRouter');
 
 const app = express();
 dotenv.config({ path: './config.env' });
 
-app.get('/', (req, res) => {
-  res.send('Hello There.');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.set('strictQuery', true);
+const url = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('Db connected successfully.');
+  });
+
+app.use(express.json());
+app.use('/user', userRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
